7d1e85921f503d923645145d7284ada1221958db
Vince Kearney <vincent.kearney@saltdna.com>
2016-09-23 17:18:33 +0100

    Forgot to import the RecyclerView into MainActivity, Andrew not to set up a card view to use for the layout

--- app/src/main/java/industries/muskaqueers/thunderechosaber/MainActivity.java
+++ app/src/main/java/industries/muskaqueers/thunderechosaber/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 //import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -79,8 +80,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public static class CounsellorViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener
-    {
+    public static class CounsellorViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         private Counsellor counsellor;
         private TextView nameTextView, ageTextView, heroTextView;
         private String name, age, hero;
