14deec9e1efd393f44175af2685b4689d223fd8c
Vince Kearney <vincent.kearney@saltdna.com>
2016-09-24 19:17:52 +0100

    Changing method names to MLA

--- app/src/main/java/industries/muskaqueers/thunderechosaber/Managers/FirebaseManager.java
+++ app/src/main/java/industries/muskaqueers/thunderechosaber/Managers/FirebaseManager.java
@@ -19,17 +19,17 @@ import industries.muskaqueers.thunderechosaber.ThunderEchoSaberApplication;
 public class FirebaseManager {
     private static final String TAG = "FirebaseManager";
     public DatabaseReference firebaseDataReference;
-    private CounsellorDatabaseHelper counsellorDatabaseHelper;
+    private CounsellorDatabaseHelper MLA_DB_Helper;
 
     public FirebaseManager() {
-        this.counsellorDatabaseHelper = new CounsellorDatabaseHelper(ThunderEchoSaberApplication.getLocalDatabaseManager());
+        this.MLA_DB_Helper = new CounsellorDatabaseHelper(ThunderEchoSaberApplication.getLocalDatabaseManager());
         this.firebaseDataReference = FirebaseDatabase.getInstance().getReference("test");
         this.firebaseDataReference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(com.google.firebase.database.DataSnapshot dataSnapshot) {
                 String dataSnapshotString = dataSnapshot.getValue().toString();
                 Log.d(TAG, "DataSnapshot value --> " + dataSnapshotString);
-                createCounsellor(dataSnapshotString);
+                createMLA(dataSnapshotString);
             }
 
             @Override
@@ -39,17 +39,14 @@ public class FirebaseManager {
         });
     }
 
-    public void createCounsellor(String jsonResponse) {
-        try {
+    public void createMLA(String jsonResponse) {
+        try { // Let's add the MLA that we just pulled down
             JSONObject jsonObject = new JSONObject(jsonResponse);
             JSONObject memberObject = jsonObject.getJSONObject("vince");
             String name = memberObject.optString("name");
             String age = memberObject.optString("age");
             String hero = memberObject.optString("hero");
-
-            this.counsellorDatabaseHelper.addCounsellor(name, age, hero);
-//            Log.i(TAG, "Counsellor count == " + this.counsellorDatabaseHelper.getAllCounsellors().size());
-//            Log.i(TAG, "The counsellor object created - " + newCounsellor + "       The hero --> " + newCounsellor.getHero());
+            this.MLA_DB_Helper.addCounsellor(name, age, hero);
         } catch (JSONException e) {
             e.printStackTrace();
         }
