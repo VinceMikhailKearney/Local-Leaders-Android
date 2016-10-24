4336e536803d6c8849360c58dfc5a21c880ddc54
Vince Kearney <vincent.kearney@saltdna.com>
2016-10-24 17:59:39 +0100

    Tracking Credentials file

---
+++ app/src/main/java/industries/muskaqueers/thunderechosaber/Utils/Credentials.java
@@ -0,0 +1,19 @@
+package industries.muskaqueers.thunderechosaber.Utils;
+
+/**
+ * Created by Andrew on 9/26/16.
+ */
+
+public class Credentials {
+
+    private static final String TWITTER_KEY = "777451185456447488-F11oGM1yG5O1VKX1ZGKwGggbDQWKucq";
+    private static final String TWITTER_SECRET = "JLJz5pYn58U9V88wJGvQNlsPkYNk1B2fwKE168SywcBTd";
+
+    public static String getTwitterKey() {
+        return TWITTER_KEY;
+    }
+
+    public static String getTwitterSecret() {
+        return TWITTER_SECRET;
+    }
+}
