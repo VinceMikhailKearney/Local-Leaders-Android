3467338d8add0378a4a3dc11e1879d0846498073
Vince Kearney <vincent.kearney@saltdna.com>
2016-10-25 21:20:31 +0100

    Trying to update the script for downloading images

--- app/src/main/java/industries/muskaqueers/thunderechosaber/Scripts/fetch_images.py
+++ app/src/main/java/industries/muskaqueers/thunderechosaber/Scripts/fetch_images.py
@@ -9,29 +9,37 @@ import getpass
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 def fetchJSONfile(str):
-    clearOutMlaImageDirectory()
-    print "Cleared out existing directory. Going to update."
-    os.chdir('app/src/main/res/drawable/mlas')
+    os.chdir('app/src/main/res/drawable/')
     print "Changed dir to store images"
     req = urllib2.Request(str)
     opener = urllib2.build_opener()
     f = opener.open(req)
     jsonResponse = json.loads(f.read())
     for i in jsonResponse:
-        print "Downloading image for -> %s" % i['MemberName']
-        downloadImageFromUrl(i['MemberImgUrl'])
+        downloadImageFromUrl(i['MemberImgUrl'], i['MemberName'])
 
-def downloadImageFromUrl(str):
-    urlLink = str
-    filename = (urlLink.split('/')[-1]).replace("_s", "")
-    urllib.urlretrieve(urlLink, filename)
+def downloadImageFromUrl(url, name):
+    filename = (url.split('/')[-1]).replace("_s", "")
+    if not mlaAlreadyDownloaded(filename):
+        print "Downloading image for -> %s" % name
+        urllib.urlretrieve(url, filename)
 
+def mlaAlreadyDownloaded(filename):
+    for the_file in os.listdir('.'):
+        file_path = os.path.join('.', the_file)
+        try:
+            if os.path.isfile(file_path):
+                if filename in the_file:
+                    return True
+                return False
+        except Exception as e:
+            print(e)
+
+# Don't use below but keeping if we need it
 def clearOutMlaImageDirectory():
-    folder = 'app/src/main/res/drawable/mlas'
+    folder = 'app/src/main/res/drawable/'
     for the_file in os.listdir(folder):
-        print the_file
         file_path = os.path.join(folder, the_file)
-        print file_path
         try:
             if os.path.isfile(file_path):
                 os.unlink(file_path)
